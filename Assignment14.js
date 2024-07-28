a3db31a897a82d6a339a54c975f38429f442727d
abdulshakoor123 <hangu3456789@gmail.com>
2024-07-28 15:22:35 +0500

    added JS files Chapter 28 and Chapter 29 problems

---
+++ Assignment14.js
@@ -0,0 +1,31 @@
+// Java Scrpts Problme of Chapter 28 and Chapter 29
+
+// Problme#1
+
+// var num = "123abc";
+// var converted = parseInt(num);
+// console.log(converted);
+
+// Problem#2 
+
+// var str1 = "45.67xyz";
+// var myStr = parseFloat(str1);
+// console.log(myStr);
+
+// Problem#3
+
+// var sttr = "123.45";
+// var conversion = Number(sttr);
+// console.log(conversion);
+
+// Problem#4
+
+// var num1 = 789
+// var myStr = num1.toString();
+// console.log(typeof myStr);
+
+// Problem#5
+// var value = "256px";
+// var toInteger = parseInt(value);
+// var toString = toInteger.toString();
+// console.log(typeof toString);
